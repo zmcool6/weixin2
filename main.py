e4f4efa2f30ae0c851ba3827930fdfca31901a7c
zmcool6 <49740643+zmcool6@users.noreply.github.com>
2022-09-30 14:28:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -185,5 +185,5 @@ if __name__ == "__main__":
         note_ch, note_en = get_ciba()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
+        send_message(user, accessToken, note_ch, note_en)
     os.system("pause")
